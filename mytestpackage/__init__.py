92abf22b689fcee42c2e84509c72c20d78f01548
gcampini <campinigil@gmail.com>
2023-03-13 14:12:51 +0100

    sol

---
+++ mytestpackage/__init__.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+__version__ = '0.1.7'
+
+def hello():
+    print('Hello World')
+    print(np.__version__)
+
+def add(a, b):
+    return a + b
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from mytestpackage import __version__
+
+setup(
+    name='mytestpackage-gcampini',
+    version=__version__,
+    description='A small example package',
+    author='Gil Campini',
+    install_requires=['numpy'],
+    packages=find_packages(exclude=['tests']),
+)
